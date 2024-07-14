bf3355605b41bdbc907bed27ca6e2e9ff8b4e37a
Yusuf Ashour <yashour@rightshero.com>
2024-07-14 06:43:38 +0300

    feat(client): add components to routes

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,6 +1,9 @@
 import Login from "../pages/Authentication/Login";
 import Register from "../pages/Authentication/Register";
+import EventsIndex from "../pages/Events/EventsIndex";
+import EventsCreate from "../pages/Events/EventsCreate";
 import React from "react";
+import { Navigate } from "react-router";
 
 
 const UNPROTECTED_ROUTES = [
@@ -15,7 +18,18 @@ const UNPROTECTED_ROUTES = [
 ];
 
 const PROTECTED_ROUTES = [
-
+    {
+        path: "/",
+        element: <EventsIndex />,
+    },
+    {
+        path: "/create",
+        element: <EventsCreate />,
+    },
+    {
+        path: "*",
+        element: <Navigate to="/" />,
+    }
 ];
 
 export { UNPROTECTED_ROUTES, PROTECTED_ROUTES };
